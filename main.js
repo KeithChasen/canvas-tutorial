7e7f078e383e27c8f639a37b529daeec516014b6
keithchasen <keithchasen89@gmail.com>
2022-09-16 23:17:38 +0300

    individual colors for particular square

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ var c = canvas.getContext('2d');
 c.fillStyle = 'rgba(243, 200, 100, .6)';
 c.fillRect(100, 100, 50, 50);
 
+c.fillStyle = 'rgba(200, 243, 100, .6)';
+c.fillRect(200, 100, 50, 50);
+
+c.fillStyle = 'rgba(200, 100, 243, .6)';
+c.fillRect(300, 100, 50, 50);
+
 c.beginPath();
 c.moveTo(50, 300);
 c.lineTo(300, 100);
