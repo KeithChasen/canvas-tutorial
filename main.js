1fb6303688ed7e9c09431db1c95f7344761c92d9
keithchasen <keithchasen89@gmail.com>
2022-09-16 23:13:06 +0300

    drawing lines

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 50, 50);
+
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle='red';
+c.stroke();
