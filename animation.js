2ae6a2626198901c578cf748ea0ba2ed1ee0bc37
keithchasen <keithchasen89@gmail.com>
2022-09-18 11:08:15 +0300

    circle bouncing with its edge not center

--- animation.js
+++ animation.js
@@ -6,17 +6,18 @@ var c = canvas.getContext('2d');
 
 
 let x = 200;
-let dx = 1; // velocity
+let dx = 5; // velocity
+const radius = 30;
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, innerWidth, innerHeight);
 
     c.beginPath();
-    c.arc(x, 300, 30, 0, Math.PI * 2, false);
+    c.arc(x, 300, radius, 0, Math.PI * 2, false);
     c.strokeStyle = 'blue';
     c.stroke();
 
-    if (x > innerWidth || x < 0) {
+    if (x + radius > innerWidth || x - radius < 0) {
         dx = -dx;
     }
 
